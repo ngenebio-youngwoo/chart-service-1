d300830aa8d184ee037327ae944d10bac8705631
youngwoo-ngenebio <youngwoo.lee@ngenebio.com>
2023-01-26 15:22:32 +0900

    ChartServiceConfiguration 구성 - application 속성 정의

--- src/main/java/com/ngenebio/msa/chart/Application.java
+++ src/main/java/com/ngenebio/msa/chart/Application.java
@@ -1,9 +1,14 @@
 package com.ngenebio.msa.chart;
 
+import com.ngenebio.msa.chart.configuration.properties.ChartServiceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableConfigurationProperties({
+        ChartServiceConfiguration.class
+})
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
---
+++ src/main/java/com/ngenebio/msa/chart/configuration/properties/ChartServiceConfiguration.java
@@ -0,0 +1,12 @@
+package com.ngenebio.msa.chart.configuration.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "chart-service")
+@Data
+public class ChartServiceConfiguration {
+    private String resultServiceUrl;
+}
